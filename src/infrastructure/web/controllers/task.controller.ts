675040244a7563106b51badcc144492f5cedda7a
Ilya Semyonov <mipselqq@gmail.com>
2025-09-05 23:38:03 +0300

    feat: strictly type Request in controller

--- src/infrastructure/web/controllers/task.controller.ts
+++ src/infrastructure/web/controllers/task.controller.ts
@@ -5,26 +5,32 @@ import { CreateTaskUseCase } from "../../../application/usecases/create-task.use
 import { GetAllTasksUseCase } from "../../../application/usecases/get-all-tasks.usecase";
 import { ensureErrorBase } from "../../errors/base-error-ensurance";
 
-// TODO: revisit error handling
-// TODO: use IOC?
+interface CreateTaskBody {
+    title: string | undefined;
+}
+
+interface CompleteTaskParams {
+    id: string | undefined;
+}
+
 export class TaskController {
     constructor(
-        private readonly crateTaskUseCase: CreateTaskUseCase,
+        private readonly createTaskUseCase: CreateTaskUseCase,
         private readonly getAllTasksUseCase: GetAllTasksUseCase,
         private readonly completeTaskUseCase: CompleteTaskUseCase,
     ) {}
 
-    createTask = async (req: Request, res: Response): Promise<void> => {
+    createTask = async (req: Request<{}, {}, CreateTaskBody>, res: Response): Promise<void> => {
         try {
-            const title  = (req as any).body.title as string | undefined;
+            const { title } = req.body;
             if (!title) {
-                throw new Error("No title provided")
+                throw new Error("No title provided");
             }
 
-            const task = await this.crateTaskUseCase.execute({ title });
+            const task = await this.createTaskUseCase.execute({ title });
             res.status(201).json(task);
         } catch (error) {
-            res.status(500).json({ message: ensureErrorBase(error).message })
+            res.status(500).json({ message: ensureErrorBase(error).message });
             throw error;
         } 
     }
@@ -40,15 +46,15 @@ export class TaskController {
         }
     }
 
-    completeTask = async (req: Request, res: Response): Promise<void> => {
+    completeTask = async (req: Request<CompleteTaskParams>, res: Response): Promise<void> => {
         try {
-            const id = (req as any).params.id as string | undefined;
+            const { id } = req.params;
             if (!id) {
-                throw new Error("Invalid id provided")
+                throw new Error("Invalid id provided");
             }
             
-            const tasks = await this.completeTaskUseCase.execute({ id });
-            res.status(200).json(tasks);
+            const task = await this.completeTaskUseCase.execute({ id });
+            res.status(200).json(task);
         } catch (error) {
             res.status(500).json({ message: ensureErrorBase(error).message });
             throw error;
